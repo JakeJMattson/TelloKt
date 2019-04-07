67027b7999c1a79e4d14a95d19268a00b1bb01dc
Jake Mattson <JakeJMattson@gmail.com>
2019-04-07 14:36:12 -0500

    Refactor and cleanup
    Rename parameters and other variables to better reflect their intent.
    Add more range checks.
    Remove dead comments.

--- src/main/kotlin/io/github/jakejmattson/tellokt/TelloKt.kt
+++ src/main/kotlin/io/github/jakejmattson/tellokt/TelloKt.kt
@@ -6,7 +6,8 @@ import java.net.InetAddress
 class TelloKt {
     companion object {
         val movementRange = 20..500
-        val rotationrange = 1..3600
+        val rotationRange = 1..3600
+        val speedRange = 1..100
     }
 
     private lateinit var socket: DatagramSocket
@@ -16,9 +17,9 @@ class TelloKt {
         get() = socket.isConnected
 
     @Throws(Exception::class)
-    fun connect(strIP: String = "192.168.10.1", port: Int = 8889) {
+    fun connect(ip: String = "192.168.10.1", port: Int = 8889) {
         socket = DatagramSocket(port)
-        socket.connect(InetAddress.getByName(strIP), port)
+        socket.connect(InetAddress.getByName(ip), port)
         sendCommand("command")
     }
     fun disconnect() = socket.close()
@@ -48,47 +49,41 @@ class TelloKt {
     fun moveDown(z: Int) = move("down", z)
 
     @Throws(IOException::class)
-    fun rotateClockwise(x: Int) = rotate("cw", x)
+    fun rotateClockwise(degrees: Int) = rotate("cw", degrees)
 
     @Throws(IOException::class)
-    fun rotateCounterClockwise(x: Int) = rotate("ccw", x)
+    fun rotateCounterClockwise(degrees: Int) = rotate("ccw", degrees)
 
     @Throws(IOException::class)
     fun flip(direction: FlipDirection) = sendCommand("flip ${direction.direction}")
 
-    /**
-     * Limit: 1-100 cm/s
-     */
     @Throws(IOException::class)
-    fun setSpeed(speed: Int) = sendCommand("speed $speed")
+    fun setSpeed(speed: Int) = validateAndSend("speed", speed, speedRange)
 
     private fun Int.toMetric() = if (!isImperial) this else Math.round((this * 2.54).toFloat())
 
     private fun move(command: String, distance: Int) = validateAndSend(command, distance.toMetric(), movementRange)
-    private fun rotate(command: String, distance: Int) = validateAndSend(command, distance, rotationrange)
+    private fun rotate(command: String, degrees: Int) = validateAndSend(command, degrees, rotationRange)
 
-    private fun validateAndSend(command: String, distance: Int, range: IntRange) =
-        if (distance in range) sendCommand("$command $distance") else "Command argument not in range!"
+    private fun validateAndSend(command: String, targetValue: Int, range: IntRange) =
+        if (targetValue in range) sendCommand("$command $targetValue") else "Command argument not in range!"
 
     @Throws(IOException::class)
-    fun sendCommand(strCommand: String): String {
-        if (strCommand.isEmpty())
-            return "empty command"
+    fun sendCommand(command: String): String {
+        if (command.isEmpty()) return "Empty command."
+        if (!socket.isConnected) return "Socket Disconnected."
 
-        if (!socket.isConnected)
-            return "disconnected"
-
-        val sendData = strCommand.toByteArray()
+        val sendData = command.toByteArray()
         val sendPacket = DatagramPacket(sendData, sendData.size, socket.inetAddress, socket.port)
         socket.send(sendPacket)
 
         val receiveData = ByteArray(1024)
         val receivePacket = DatagramPacket(receiveData, receiveData.size)
         socket.receive(receivePacket)
-        val ret = String(receivePacket.data)
 
-        println("$strCommand: $ret")
-        return ret
+        val response = String(receivePacket.data)
+        println("$command: $response")
+        return response
     }
 }
 
@@ -97,7 +92,6 @@ enum class FlipDirection(val direction: String) {
     RIGHT("r"),
     FORWARD("f"),
     BACKWARD("b"),
-
     BACK_LEFT("bl"),
     BACK_RIGHT("rb"),
     FRONT_LEFT("fl"),
